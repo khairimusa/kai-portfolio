ccbe4a6743b4cc3de86551b22e20ffdcf2592221
Khairi Musa <khairimusa60@gmail.com>
2021-09-07 21:23:01 +0800

    Add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Hero from './components/hero/Hero';
 import TopNavBar from './components/top-nav-bar/TopNavBar';
+import styled,{ThemeProvider} from "styled-components";
+import {lightTheme, darkTheme, GlobalStyles} from './theme';
 
+
+const StyledApp = styled.div ``;
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const themeToggler = function() {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  }
   return (
-    <div>
-      <TopNavBar/>
-      <Hero/>
-    </div>
+    <ThemeProvider theme={theme === "light" ?  lightTheme : darkTheme }>
+      <StyledApp>
+        <TopNavBar/>
+        <Hero/>
+      </StyledApp>
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components"
+
+export const lightTheme = {
+    body: '#fff',
+    font: '#000'
+}
+
+export const darkTheme = {
+    body: '#000',
+    font: '#fff'
+}
+
+export const GlobalStyles = createGlobalStyle`
+
+    body {
+
+        background-color: ${(props) => props.theme.body};
+    
+    }
+
+`;
