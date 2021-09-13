050e0cdc5d75a6f68f00a656543d253623fd752e
Khairi Musa <khairimusa60@gmail.com>
2021-09-14 02:29:33 +0800

    update link and scroll bug

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -6,10 +6,10 @@ function Info() {
   return (
 <div className="container">
     <div>
-        <a href="https://www.linkedin.com/in/khairimusa/" target="_blank"><FaLinkedin size={70}/></a>
+        <a href="https://www.linkedin.com/in/khairimusa/" target="_blank" rel="noreferrer"><FaLinkedin size={70}/></a>
     </div>  
     <div>
-        <a href="https://github.com/khairimusa" target="_blank"><FaGithub size={70}/></a>
+        <a href="https://github.com/khairimusa" target="_blank" rel="noreferrer"><FaGithub size={70}/></a>
     </div>
 </div>
   );
