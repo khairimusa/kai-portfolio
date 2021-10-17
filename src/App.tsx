b1580ae06255ff0b481a80b2daec2b38b08b0eb2
Khairi Musa <khairimusa60@gmail.com>
2021-10-18 01:01:01 +0800

    Add tailwind css & refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
 import './App.css';
 import Hero from './components/hero/Hero';
 import TopNavBar from './components/top-nav-bar/TopNavBar';
-import styled,{ThemeProvider} from "styled-components";
-import {lightTheme, darkTheme} from './theme';
 import Links from './components/links/Links';
 import Info from './components/info/Info';
 
-
-const StyledApp = styled.div ``;
-
 function App() {
-  const [theme, setTheme] = useState("dark");
-  // eslint-disable-next-line
-  const themeToggler = function() {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  }
   return (
-    <ThemeProvider theme={theme === "light" ?  lightTheme : darkTheme }>
-      <StyledApp>
-        <TopNavBar/>
-        <Hero/>
-        <Links/>
-        <Info/>
-      </StyledApp>
-    </ThemeProvider>
-
+    <div>
+      <TopNavBar/>
+      <Hero/>
+      <Links/>
+      <Info/>
+    </div>
   );
 }
 
--- src/components/top-nav-bar/TopNavBar.tsx
+++ src/components/top-nav-bar/TopNavBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './TopNavBar.css';
 
 function TopNavBar() {
